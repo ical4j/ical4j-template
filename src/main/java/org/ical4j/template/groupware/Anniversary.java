901b5fef9fbfe6faabc1c6436bb8848d307a3510
Ben Fortuna <fortuna@micronode.com>
2024-01-05 11:29:29 +1100

    Updated groupware templates

---
+++ src/main/java/org/ical4j/template/groupware/Anniversary.java
@@ -0,0 +1,50 @@
+package org.ical4j.template.groupware;
+
+import net.fortuna.ical4j.extensions.property.Repeats;
+import net.fortuna.ical4j.model.component.VEvent;
+import org.ical4j.template.AbstractTemplate;
+
+import java.time.LocalDate;
+
+import static net.fortuna.ical4j.model.DateTimePropertyModifiers.DTSTART;
+import static net.fortuna.ical4j.model.RecurrencePropertyModifiers.RRULE;
+
+/**
+ * Creates a recurring {@link VEvent} representing a birthday/anniversary, etc.
+ */
+public class Anniversary extends AbstractTemplate<VEvent> {
+
+    private Repeats<?> schedule;
+
+    private LocalDate date;
+
+    public Anniversary() {
+        super(VEvent.class);
+    }
+
+    public Anniversary(Class<? extends VEvent> typeClass) {
+        super(typeClass);
+    }
+
+    public <T extends VEvent> Anniversary(T prototype) {
+        super(prototype.getClass());
+        setPrototype(prototype);
+    }
+
+    public Anniversary withRepeats(Repeats<?> schedule) {
+        this.schedule = schedule;
+        return this;
+    }
+
+    public Anniversary withDate(LocalDate date) {
+        this.date = date;
+        return this;
+    }
+
+    @Override
+    public VEvent apply(VEvent vEvent) {
+        vEvent.with(DTSTART, date);
+        vEvent.with(RRULE, schedule);
+        return vEvent;
+    }
+}

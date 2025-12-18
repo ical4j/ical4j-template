9ee046bd28da78260ee0d8269eb67a5a086455fe
Ben Fortuna <ben.fortuna@team.telstra.com>
2025-12-19 10:54:01 +1100

    Add EventModel class to handle iCalendar events

--- src/main/java/org/ical4j/template/model/EventModel.java
+++ src/main/java/org/ical4j/template/model/EventModel.java
@@ -1,4 +1,37 @@
 package org.ical4j.template.model;
 
+import net.fortuna.ical4j.model.component.VEvent;
+
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Objects;
+
 public class EventModel {
+
+    private final VEvent event;
+
+    public EventModel(VEvent event) {
+        this.event = event;
+    }
+
+    public String getSummary() {
+        return Objects.requireNonNull(event.getSummary()).getValue();
+    }
+
+    public String getStart() {
+        return DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM).format(
+                Objects.requireNonNull(event.getDateTimeStart()).getDate());
+    }
+
+    public  String getEnd() {
+        return DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM).format(
+                Objects.requireNonNull(event.getDateTimeEnd()).getDate());
+    }
+
+    @Override
+    public String toString() {
+        return "Summary: " + getSummary() + "\n" +
+                "Start: " + getStart() + "\n" +
+                "End: " + getEnd() + "\n";
+    }
 }

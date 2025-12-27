6489f0f1c8e8f9100df539fc9b3e6711aa1e1245
Ben Fortuna <fortuna@nodelogic.au>
2025-12-28 10:11:33 +1100

    Add DateUtils interface for date manipulation; implement methods to retrieve week dates and create date periods

---
+++ src/main/java/org/ical4j/template/util/DateUtils.java
@@ -0,0 +1,50 @@
+package org.ical4j.template.util;
+
+import net.fortuna.ical4j.model.Period;
+
+import java.time.LocalDate;
+import java.time.temporal.Temporal;
+import java.util.List;
+
+/*
+ * Copyright (c) 2025, Ben Fortuna
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  o Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ *  o Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ *  o Neither the name of Ben Fortuna nor the names of any other contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+public interface DateUtils {
+
+    static List<LocalDate> getWeekDates(LocalDate date) {
+        LocalDate startOfWeek = date.minusDays(date.getDayOfWeek().getValue() - 1);
+        return startOfWeek.datesUntil(startOfWeek.plusDays(7)).toList();
+    }
+
+    static Period<Temporal> getPeriod(LocalDate date) {
+        return new Period<>(date.atStartOfDay(), date.plusDays(1).atStartOfDay());
+    }
+}

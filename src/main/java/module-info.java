248dfec0ce82f1424b9d9d0379dd9b93954dcca1
Ben Fortuna <fortuna@nodelogic.au>
2025-12-29 17:32:39 +1100

    Remove unused ical4j dependencies and update versions in configuration files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,8 @@ module ical4j.template {
     requires java.base;
     requires ical4j.core;
     requires ical4j.vcard;
-    requires ical4j.extensions;
     requires gg.jte.runtime;
     requires gg.jte;
-    requires ical4j.connector.api;
     requires org.slf4j;
 
     exports org.ical4j.template;
